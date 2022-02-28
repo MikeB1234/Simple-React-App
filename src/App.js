461aa073181ffa9eb7022e08c84a4a7d6b7877e9
MikeB1234 <mbacha2345@gmail.com>
2022-02-28 10:44:20 -0500

    destructuring useState hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
-function SecretComponent() {
-  return (
-    <h1>Secret info for authorized users only.</h1>
-  )
-}
+function App() {
+  const [emotion, setEmotion] = useState("happy");
 
-function RegularComponent() {
-  return (
-    <h1>Everyone can see this component.</h1>
-  )
-}
-
-function App({ authorized }) {
   return (
     <>
-      {authorized ? <SecretComponent /> : <RegularComponent />}
+      <h1>Hello, current emotion is {emotion}.</h1>
+      <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
     </>
   )
 }
