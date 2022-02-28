b2f154b15dc24b2b27d7fff9590e149403738ff2
MikeB1234 <mbacha2345@gmail.com>
2022-02-28 11:24:22 -0500

    Fetch and Display data from an API using hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React, { useReducer } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 
-function App() {
-  const [checked, toggle] = useReducer(
-    (checked) => !checked, false);
+//https://api.github.com/users/MikeB1234
 
-  return (
-    <>
-      <input type="checkbox" value={checked} onChange={toggle} />
-      <p>{checked ? "checked" : "unchecked"} </p>
-    </>
-  )
+function App({ login }) {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+
+  if (data) {
+    return (
+      <div>
+        <h1>{data.name}</h1>
+        <p>{data.html_url}</p>
+        <img alt={data.login} src={data.avatar_url} />
+      </div>
+    );
+  }
+
+  return <div>No user data.</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from "./App"
 
 ReactDOM.render(
-  <App />,
+  <App login="MikeB1234" />,
   document.getElementById('root')
 );
